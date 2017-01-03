273d90368c46c092e226ee6688995eacb5950c4d
hduser <hduser@Hsus-MacBook-Pro.local>
2017-01-03 17:40:20 +0800

    More work on main screen list view of shared food

--- app/src/main/java/com/bille/group3/food_e/MainMapFragment.java
+++ app/src/main/java/com/bille/group3/food_e/MainMapFragment.java
@@ -6,13 +6,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Alan on 2017/01/01.
+ *
+ * Will (for now) handle both the List display and Map display.
  */
 
 public class MainMapFragment extends Fragment{
+
+    private ArrayList<SharedFood> sharedFoods = new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        sharedFoods.add(new SharedFood("Apples for sale 30% off", new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime(),
+                new GregorianCalendar(2014, Calendar.FEBRUARY, 23).getTime(), "Shilin Carrefour", "filepath"));
+
+        sharedFoods.add(new SharedFood("Oranges for sale 30% off", new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime(),
+                new GregorianCalendar(2014, Calendar.FEBRUARY, 23).getTime(), "Shilin Carrefour", "filepath"));
+
+        sharedFoods.add(new SharedFood("Peaches for sale 30% off", new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime(),
+                new GregorianCalendar(2014, Calendar.FEBRUARY, 23).getTime(), "Shilin Carrefour", "filepath"));
+
+        sharedFoods.add(new SharedFood("Free pizza for everyone", new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime(),
+                new GregorianCalendar(2014, Calendar.FEBRUARY, 18).getTime(), "NTU CSIE Building", "filepath"));
+
         return inflater.inflate(R.layout.fragment_main_map, container, false);
     }
 }
---
+++ app/src/main/java/com/bille/group3/food_e/SharedFoodArrayAdapter.java
@@ -0,0 +1,67 @@
+package com.bille.group3.food_e;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hduser on 1/3/17.
+ */
+
+public class SharedFoodArrayAdapter extends ArrayAdapter<SharedFood> {
+
+    private Context context;
+    private List<SharedFood> sharedFoodsList;
+
+    public SharedFoodArrayAdapter(Context context, int resource, ArrayList<SharedFood> objects) {
+        super(context, resource, objects);
+
+        this.context = context;
+        this.sharedFoodsList = objects;
+    }
+
+    //called when rendering the list
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        //get the property we are displaying
+        SharedFood foodItem = sharedFoodsList.get(position);
+
+        //get the inflater and inflate the XML layout for each item
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.main_list_object, null);
+
+        TextView description = (TextView) view.findViewById(R.id.listFoodDescription);
+        TextView expire = (TextView) view.findViewById(R.id.listFoodExpireDate);
+        TextView location = (TextView) view.findViewById(R.id.listFoodLocation);
+        ImageView image = (ImageView) view.findViewById(R.id.image);
+
+        //set address and description
+        location.setText(foodItem.getLocation());
+
+        //display trimmed excerpt for description
+        int descriptionLength = foodItem.getDescription().length();
+        if(descriptionLength >= 100){
+            String descriptionTrim = foodItem.getDescription().substring(0, 100) + "...";
+            description.setText(descriptionTrim);
+        }else{
+            description.setText(foodItem.getDescription());
+        }
+
+        //set price and rental attributes
+        expire.setText(foodItem.getExpiration().toString());
+
+        //get the image associated with this property
+        int imageID = context.getResources().getIdentifier(foodItem.getImage(), "drawable", context.getPackageName());
+        image.setImageResource(imageID);
+
+        return view;
+    }
+}
